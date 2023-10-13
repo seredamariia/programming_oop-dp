91803bbf85121a4e43a52927e32b2a083634f19f
Mariia Sereda <seredamariiav@gmail.com>
2023-10-13 21:30:19 +0300

    Initial structure of Lab_1

---
+++ Lab_1/Lab_1/Program.cs
@@ -0,0 +1,514 @@
+﻿using System;
+
+public class Employee : IDisposable
+{
+    // Для перевірки, чи викликався вже метод Dispose().
+    private bool disposed = false;
+
+    private string name;
+    private int age;
+    private string phoneNumber;
+    private double salary;
+
+    public Employee()
+    {
+        if (Program.access_level == "user")
+            Console.WriteLine("You do not have rights to create new Employee");
+        else
+        {
+            name = "Undefined employee";
+            phoneNumber = "+380XXXXXXXXX";
+            Console.WriteLine("Employee initial constructor worked successfully");
+        }
+    }
+
+    public Employee(string name, int age, string phoneNumber, double salary)
+    {
+        if (Program.access_level == "user")
+            Console.WriteLine("You do not have rights to create new Employee");
+        else
+        {
+            Name = name;
+            Age = age;
+            PhoneNumber = phoneNumber;
+            Salary = salary;
+            Console.WriteLine("Employee parametric constructor worked successfully");
+        }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Name");
+            else if (value == null)
+                Console.WriteLine("Name cannot be empty");
+            else
+                name = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            if (Program.access_level == "user")
+            {
+                Console.WriteLine("You do not have rights to get Age");
+                return 0;
+            }
+            else
+                return age;
+        }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Age");
+            else if (value < 16 || value > 99)
+                Console.WriteLine("Age must be in between 16 and 99");
+            else
+                age = value;
+        }
+    }
+
+    public string PhoneNumber
+    {
+        get { return phoneNumber; }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Phone Number");
+            else if (value == null)
+                Console.WriteLine("Phone Number cannot be empty");
+            else if (!value.Contains("+380"))
+                Console.WriteLine("Phone Number must start with +380");
+            else
+                phoneNumber = value;
+        }
+    }
+
+    public double Salary
+    {
+        get
+        {
+            if (Program.access_level == "user")
+            {
+                Console.WriteLine("You do not have rights to get Salary");
+                return 0;
+            }
+            else
+                return salary;
+        }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Salary");
+            else if (value < 6700.00)
+                Console.WriteLine("Salary cannot be less than 6700 UAH");
+            else
+                salary = value;
+        }
+    }
+
+    public virtual void DisplayMainSkill()
+    {
+        Console.WriteLine("Employee's main skill depends on theirs position");
+    }
+
+    public void DisplayFullInfo()
+    {
+        if (Program.access_level == "admin")
+            Console.WriteLine($"{name,-30}   {age,2} y.o.   {phoneNumber,13}   {salary,5} UAH");
+        else
+            Console.WriteLine("You do not have rights to see Full Info");
+    }
+
+    public void Dispose()
+    {
+        // Виклик допоміжного методу. "true" - якщо очищення було викликане користувачем об'єкта. 
+        CleanUp(true);
+        // Подавлення фіналізації.
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void CleanUp(bool disposing)
+    {
+        // Перевірка, чи було виконано очищення.
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                Console.WriteLine("Disposing managed resourses");
+            }
+            Console.WriteLine("Disposing unmanaged resourses");
+            disposed = true;
+        }
+    }
+
+    ~Employee()
+    {
+        // Виклик допоміжного методу.
+        CleanUp(false);
+        Console.WriteLine("Employee destroyed");
+    }
+}
+
+public class Developer : Employee
+{
+    private string name;
+    private int age;
+    private string phoneNumber;
+    private double salary;
+
+    public Developer(string name, int age, string phoneNumber, double salary) : base(name, age, phoneNumber, salary)
+    {
+        Console.WriteLine("Developer parametric constructor worked successfully");
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Name");
+            else if (value == null)
+                Console.WriteLine("Name cannot be empty");
+            else
+                name = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            if (Program.access_level == "user")
+            {
+                Console.WriteLine("You do not have rights to get Age");
+                return 0;
+            }
+            else
+                return age;
+        }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Age");
+            else if (value < 16 || value > 99)
+                Console.WriteLine("Age must be in between 16 and 99");
+            else
+                age = value;
+        }
+    }
+
+    public string PhoneNumber
+    {
+        get { return phoneNumber; }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Phone Number");
+            else if (value == null)
+                Console.WriteLine("Phone Number cannot be empty");
+            else if (!value.Contains("+380"))
+                Console.WriteLine("Phone Number must start with +380");
+            else
+                phoneNumber = value;
+        }
+    }
+
+    public double Salary
+    {
+        get
+        {
+            if (Program.access_level == "user")
+            {
+                Console.WriteLine("You do not have rights to get Salary");
+                return 0;
+            }
+            else
+                return salary;
+        }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Salary");
+            else if (value < 6700.00)
+                Console.WriteLine("Salary cannot be less than 6700 UAH");
+            else
+                salary = value;
+        }
+    }
+
+    public override void DisplayMainSkill()
+    {
+        Console.WriteLine("Developer's main skill is writting code");
+    }
+}
+
+public class Designer : Employee
+{
+    private string name;
+    private int age;
+    private string phoneNumber;
+    private double salary;
+
+    public Designer(string name, int age, string phoneNumber, double salary) : base(name, age, phoneNumber, salary)
+    {
+        Console.WriteLine("Designer parametric constructor worked successfully");
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Name");
+            else if (value == null)
+                Console.WriteLine("Name cannot be empty");
+            else
+                name = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            if (Program.access_level == "user")
+            {
+                Console.WriteLine("You do not have rights to get Age");
+                return 0;
+            }
+            else
+                return age;
+        }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Age");
+            else if (value < 16 || value > 99)
+                Console.WriteLine("Age must be in between 16 and 99");
+            else
+                age = value;
+        }
+    }
+
+    public string PhoneNumber
+    {
+        get { return phoneNumber; }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Phone Number");
+            else if (value == null)
+                Console.WriteLine("Phone Number cannot be empty");
+            else if (!value.Contains("+380"))
+                Console.WriteLine("Phone Number must start with +380");
+            else
+                phoneNumber = value;
+        }
+    }
+
+    public double Salary
+    {
+        get
+        {
+            if (Program.access_level == "user")
+            {
+                Console.WriteLine("You do not have rights to get Salary");
+                return 0;
+            }
+            else
+                return salary;
+        }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Salary");
+            else if (value < 6700.00)
+                Console.WriteLine("Salary cannot be less than 6700 UAH");
+            else
+                salary = value;
+        }
+    }
+
+    public override void DisplayMainSkill()
+    {
+        Console.WriteLine("Designer's main skill is creating UX/UI design");
+    }
+}
+
+public class Tester : Employee
+{
+    private string name;
+    private int age;
+    private string phoneNumber;
+    private double salary;
+
+    public Tester(string name, int age, string phoneNumber, double salary) : base(name, age, phoneNumber, salary)
+    {
+        Console.WriteLine("Tester parametric constructor worked successfully");
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Name");
+            else if (value == null)
+                Console.WriteLine("Name cannot be empty");
+            else
+                name = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            if (Program.access_level == "user")
+            {
+                Console.WriteLine("You do not have rights to get Age");
+                return 0;
+            }
+            else
+                return age;
+        }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Age");
+            else if (value < 16 || value > 99)
+                Console.WriteLine("Age must be in between 16 and 99");
+            else
+                age = value;
+        }
+    }
+
+    public string PhoneNumber
+    {
+        get { return phoneNumber; }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Phone Number");
+            else if (value == null)
+                Console.WriteLine("Phone Number cannot be empty");
+            else if (!value.Contains("+380"))
+                Console.WriteLine("Phone Number must start with +380");
+            else
+                phoneNumber = value;
+        }
+    }
+
+    public double Salary
+    {
+        get
+        {
+            if (Program.access_level == "user")
+            {
+                Console.WriteLine("You do not have rights to get Salary");
+                return 0;
+            }
+            else
+                return salary;
+        }
+        set
+        {
+            if (Program.access_level == "user")
+                Console.WriteLine("You do not have rights to set Salary");
+            else if (value < 6700.00)
+                Console.WriteLine("Salary cannot be less than 6700 UAH");
+            else
+                salary = value;
+        }
+    }
+
+    public override void DisplayMainSkill()
+    {
+        Console.WriteLine("Tester's main skill is testing software");
+    }
+}
+
+public class Authorization
+{
+    private static Authorization instance;
+
+    private string login;
+    private string password;
+    private static string correctLogin = "admin";
+    private static string correctPassword = "PtahaFred";
+    private string access_level;
+
+    private Authorization()
+    {
+        login = "user";
+        access_level = "user";
+    }
+
+    private Authorization(string login, string password)
+    {
+        if (login == correctLogin && password == correctPassword)
+            access_level = "admin";
+        else
+        {
+            Console.WriteLine("Login or password is invalid. Your current access level is [user]");
+            access_level = "user";
+        }
+
+    }
+
+    public static Authorization GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Authorization();
+        }
+        return instance;
+    }
+
+    public static Authorization GetInstance(string login, string password)
+    {
+        if (instance == null)
+        {
+            instance = new Authorization(login, password);
+        }
+        return instance;
+    }
+
+    public string GetAccessLevel()
+    {
+        return access_level;
+    }
+}
+
+class Program
+{
+    public static string access_level;
+
+    static void Main(string[] args)
+    {
+        Authorization authorization = null;
+        Console.WriteLine("Your current access level is [user] and you have limited rights. Do you want to change access level to [admin] and have unlimited rights?");
+        Console.WriteLine("Enter [YES] or [NO]");
+        string answer = Console.ReadLine();
+        if (answer.ToUpper() == "NO")
+            authorization = Authorization.GetInstance();
+        else if (answer.ToUpper() == "YES")
+        {
+            Console.WriteLine("Enter login:");
+            string login = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine();
+            authorization = Authorization.GetInstance(login, password);
+        }
+        access_level = authorization.GetAccessLevel();
+    }
+}
